c56f9cc1ed0cfe9ce8811501453f0e3d87457faf
zanderlx <lsaplan@gmail.com>
2019-02-14 13:02:17 -0800

    Updated board display

--- Assignment 4/n-queens.py
+++ Assignment 4/n-queens.py
@@ -1,5 +1,6 @@
 import sys
 import random
+from Board import Board
 
 # Number of queens
 n = sys.argv[1]
@@ -10,7 +11,13 @@ states = []
 for state in range(int(k)):
     random_queens = ""
     for queen in range(int(n)):
-        random_queens += str(random.randint(0, int(n)))
+        random_queens += str(random.randint(0, int(n) - 1))
     states.append(random_queens)
 
-print("\n".join(states))
+for element in states:
+    board = Board(int(n))
+    print(element)
+    board.populate(element)
+    board.display()
+    print("\n")
+    
