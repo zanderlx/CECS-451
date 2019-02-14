b75d04f0cfaf32cd769f823526a2c4742bf3bc7f
Breeland Abrams <breeland.abrams@gmail.com>
2019-02-14 11:48:39 -0800

    update populate method

--- Assignment 4/Board.py
+++ Assignment 4/Board.py
@@ -2,10 +2,10 @@ class Board:
     def __init__(self, queens: int):
         self.spaces = [["-" for i in range(queens)] for k in range(queens)]
 
-    def populate(self, positions: list):
+    def populate(self, positions: str):
         i = 0
-        for n in positions:
-            (self.spaces[n])[i] = "Q"
+        for n in range(len(positions)):
+            (self.spaces[int(positions[n])])[i] = "Q"
             i += 1
 
     def display(self):
@@ -15,5 +15,5 @@ class Board:
 
 if __name__ == "__main__":
     b = Board(8)
-    b.populate([2, 1, 4, 3, 2, 1, 0, 2])
+    b.populate("21432101")
     b.display()
