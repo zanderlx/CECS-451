1ebfa52cd8cf9df57584fa1a8ca1a141c318e179
zanderlx <lsaplan@gmail.com>
2019-02-19 11:02:34 -0800

    Added fitness function

--- Assignment 4/Board.py
+++ Assignment 4/Board.py
@@ -1,19 +1,76 @@
+
 class Board:
+
     def __init__(self, queens: int):
-        self.spaces = [["-" for i in range(queens)] for k in range(queens)]
+        self.spaces = [["-" for col in range(queens)] for row in range(queens)]
+
 
     def populate(self, positions: str):
-        i = 0
-        for n in range(len(positions)):
-            (self.spaces[int(positions[n])])[i] = "Q"
-            i += 1
+        col = 0
+        for row in range(len(positions)):
+            (self.spaces[int(positions[row]) - 1])[col] = "Q"
+            col += 1
+
 
     def display(self):
-        for l in self.spaces:
-            print(l)
+        for element in self.spaces:
+            print(element)
 
+
+    def check_topright(self, row, col):
+        result = 0
+        for _ in zip(range(row, -1, -1), range(col, 7, 1)):
+            col += 1
+            row -= 1
+            if (self.spaces[row])[col] is "Q":
+                result += 1
+                # print("     top right", row, col)
+        return result
+
+
+    def check_middleright(self, row, col):
+        result = 0
+        
+        for col in range(col, 7):
+            col += 1
+            if (self.spaces[row])[col] is "Q":
+                result += 1
+                # print("     middle right", row, col)
+        return result
+
+
+    def check_bottomright(self, row, col):
+        result = 0
+        for _ in zip(range(row, 7), range(col, 7)):
+            col += 1
+            row += 1
+            if (self.spaces[row])[col] is "Q":
+                result += 1
+                # print("     bottom right", row, col)
+        return result
+        
+
+    def fitness(self, state):
+        result = 0
+        col = 0
+        for digit in state:
+            if col is 7: break
+            # print("Column", col)
+            row = int(digit) - 1
+            result += self.check_topright(row, col)
+            result += self.check_middleright(row, col)
+            result += self.check_bottomright(row, col)
+            col += 1 
+        # print("Attacking Queens:", result)
+        print("Non-Attacking Queens:", 28 - result)
 
 if __name__ == "__main__":
     b = Board(8)
-    b.populate("21432101")
+    
+    # b.populate("24748552")
+    # b.populate("32752411")
+    # b.populate("24415124")
+    b.populate("32543213")
     b.display()
+    b.fitness("32543213")
+    # b.populate("01234567890123456780123456789012345678")
--- Assignment 4/n-queens.py
+++ Assignment 4/n-queens.py
@@ -2,22 +2,26 @@ import sys
 import random
 from Board import Board
 
-# Number of queens
-n = sys.argv[1]
-# Number of states
-k = sys.argv[2]
+def set_random_queens(n, k):
+    states = []
+    for _ in range(int(k)):
+        random_queens = ""
+        for _ in range(int(n)):
+            random_queens += str(random.randint(1, int(n) - 1))
+        states.append(random_queens)
+    return states
 
-states = []
-for state in range(int(k)):
-    random_queens = ""
-    for queen in range(int(n)):
-        random_queens += str(random.randint(0, int(n) - 1))
-    states.append(random_queens)
+if __name__ == "__main__":
+    # Number of queens
+    n = sys.argv[1]
+    # Number of states
+    k = sys.argv[2]
 
-for element in states:
-    board = Board(int(n))
-    print(element)
-    board.populate(element)
-    board.display()
-    print("\n")
-    
+    fitness_values = []
+    for state in set_random_queens(n, k):
+        board = Board(int(n))
+        board.populate(state)
+        board.display()
+        board.fitness(state)
+        print("\n")
+        
