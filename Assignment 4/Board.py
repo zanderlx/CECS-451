033c00f705754924383e1ae743e69d9b7e81641d
zanderlx <lsaplan@gmail.com>
2019-02-19 23:39:18 -0800

    Updated Board display method

--- Assignment 4/Board.py
+++ Assignment 4/Board.py
@@ -4,8 +4,7 @@ class Board:
 
     def __init__(self, queens: int):
         self.queens = queens
-        self.spaces = [["-" for col in range(queens)] for row in range(queens)]
-
+        self.spaces = [['-' for col in range(queens)] for row in range(queens)]
 
     def populate(self, positions: list):
         col = 0
@@ -13,69 +12,18 @@ class Board:
             (self.spaces[int(positions[row]) - 1])[col] = "Q"
             col += 1
 
-
     def display(self):
         for element in self.spaces:
-            print(element)
-
-
-    def check_topright(self, row, col):
-        result = 0
-        for _ in zip(range(row, -1, -1), range(col, self.queens - 1, 1)):
-            col += 1
-            row -= 1
-            if (self.spaces[row])[col] is "Q":
-                result += 1
-                # print("     top right", row, col)
-        return result
-
-
-    def check_middleright(self, row, col):
-        result = 0
-        
-        for col in range(col, self.queens - 1):
-            col += 1
-            if (self.spaces[row])[col] is "Q":
-                result += 1
-                # print("     middle right", row, col)
-        return result
-
-
-    def check_bottomright(self, row, col):
-        result = 0
-        for _ in zip(range(row, self.queens - 1), range(col, self.queens - 1)):
-            col += 1
-            row += 1
-            if (self.spaces[row])[col] is "Q":
-                result += 1
-                # print("     bottom right", row, col)
-        return result
-        
-
-    def fitness(self, state):
-        result = 0
-        col = 0
-        for digit in state:
-            if col is self.queens - 1: break
-            # print("Column", col)
-            row = int(digit) - 1
-            result += self.check_topright(row, col)
-            result += self.check_middleright(row, col)
-            result += self.check_bottomright(row, col)
-            col += 1 
-        # print("Attacking Queens:", result)
-        print("Non-Attacking Queens:", self.combination(self.queens) - result)
-
-    
-    def combination(self, queens: int):
-        return math.factorial(queens) / (math.factorial(queens - 2) * math.factorial(2))
+            print("[", " ".join(element), "]")
 
 if __name__ == "__main__":
-    b = Board(19)
-
+    test = "24415124"
+    b = Board(len(test))
+    b.populate(test)
+    b.display()
+    # test = '3142'
     # b.populate("24748552")
     # b.populate("32752411")
     # b.populate("24415124")
     # b.fitness("32543213")
-    b.populate("1234567891234567891")
-    b.display()
+    # b.populate("1234567891234567891")
